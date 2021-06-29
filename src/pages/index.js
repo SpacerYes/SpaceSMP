ffd06af722eb7878e444f584fc7edc21c3a3b798
SpacerYes <llamapr00@gmail.com>
2021-06-28 23:36:44 -0400

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1> IP Game.hotcocoabrewed.coffee:25577</h1>
+
+    <p>----------------------------------------------------------------------------------------------------------------------------------------</p>
+    <p>current mods installed none
+      
+    </p>
+
+    <p>Rules
+    </p>
+    <p>----------------------------------------------------------------------------------------------------------------------------------------</p>
+    <p>Do not steal/grief. Just dont touch something which doesn't belong to you.
+Use common sense. Don't enter another player's area and annoy them while they are doing somthing/playing, unless you have their express permission to be in the area.
+No killing. Don't kill other players, their pets/animals, or any name tagged mob.
+PVP only with the consent of another player (s). Consent must be given in the chat in order to protect both parties from mis-information.
+All cheats and exploits are forbidden. Anything which gives an unfair advantage over other players. If you have found a bug/exploit please report it on the Discord server.
+Sensible use of the SMP. Don't settle next to another player without their express permission given in the minecraft chat, this helps provide members with the freedom to have large builds
+</p>
+<p>Just have fun:) but follow the rules</p>
+
+    <p>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
